43d6c5bbc162ea725ef5b498b907b11aaee754dd
HenriquePD123 <henrique.queiroz@pdbomdespacho.com.br>
2024-08-13 18:48:35 -0300

    Add files via upload

---
+++ python modulo 2/2.2/aula2_questao1.py
@@ -0,0 +1,6 @@
+num1 = 5
+num2 = 2
+print(type(num1))
+print(type(num2))
+resultado = num1 / num2
+print(type(resultado))
---
+++ python modulo 2/2.2/aula2_questao2.py
@@ -0,0 +1,6 @@
+texto = "o resultado é"
+num1 = 10
+num2 = 3.5
+resultado = num1 + num2
+print(texto, num1, num2, resultado)
+print(type(texto), type(num1), type(num2), type(resultado))
